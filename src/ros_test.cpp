cf3ceaf8f914507a953103b844573c9ec3319678
haohuanw <haohuanw@umich.edu>
2016-03-29 22:17:35 -0400

    add cloud viewer

--- src/ros_test.cpp
+++ src/ros_test.cpp
@@ -13,6 +13,11 @@
 
 #include <opencv2/opencv.hpp>
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+
 #include <ros/ros.h>
 #include <ros/spinner.h>
 #include <sensor_msgs/CameraInfo.h>
@@ -34,7 +39,8 @@ class Receiver{
     boost::mutex lock;
 
     const std::string topicColor, topicDepth;
-    bool updateImage;
+    bool getImage;
+    bool updateCloud;
     bool running;
     size_t frame;
     unsigned int queueSize;
@@ -44,7 +50,7 @@ class Receiver{
     cv::Mat lookupX, lookupY;
 
     typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ExactSyncPolicy;
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ApproximateSyncPolicy;
+
     std::vector<int> params;
     ros::NodeHandle nh;
     ros::AsyncSpinner spinner;
@@ -53,12 +59,17 @@ class Receiver{
     message_filters::Subscriber<sensor_msgs::CameraInfo> *subCameraInfoColor, *subCameraInfoDepth;
     message_filters::Synchronizer<ApproximateSyncPolicy> *syncApproximate;
 
-    boost::thread image_viewer_thread;
+    boost::thread get_image_thread;
+    boost::thread process_thread;
+
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud;
+    pcl::PCDWriter writer; 
+
     std::ostringstream oss;
     public:
 
     Receiver(const std::string &topicColor, const std::string &topicDepth)
-        :topicColor(topicColor), topicDepth(topicDepth), nh("~"), spinner(0), it(nh), updateImage(false), running(false),frame(0){
+        :topicColor(topicColor), topicDepth(topicDepth), nh("~"), spinner(0), it(nh), getImage(false), updateCloud(false), running(false),frame(0){
             queueSize = 5;
             cameraMatrixColor = cv::Mat::zeros(3, 3, CV_64F);
             cameraMatrixDepth = cv::Mat::zeros(3, 3, CV_64F);
@@ -87,17 +98,25 @@ class Receiver{
         std::string topicCameraInfoColor = topicColor.substr(0, topicColor.rfind('/')) + "/camera_info";
         std::string topicCameraInfoDepth = topicDepth.substr(0, topicDepth.rfind('/')) + "/camera_info";
 
-        image_transport::TransportHints hints("compressed");
+        image_transport::TransportHints hints("raw");
         subImageColor = new image_transport::SubscriberFilter(it, topicColor, queueSize, hints);
         subImageDepth = new image_transport::SubscriberFilter(it, topicDepth, queueSize, hints);
         subCameraInfoColor = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, topicCameraInfoColor, queueSize);
         subCameraInfoDepth = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, topicCameraInfoDepth, queueSize);
 
-        syncApproximate = new message_filters::Synchronizer<ApproximateSyncPolicy>(ApproximateSyncPolicy(queueSize), *subImageColor, *subImageDepth, *subCameraInfoColor, *subCameraInfoDepth);
-        syncApproximate->registerCallback(boost::bind(&Receiver::callback, this, _1, _2, _3, _4));
+        syncExact = new message_filters::Synchronizer<ExactSyncPolicy>(ExactSyncPolicy(queueSize), *subImageColor, *subImageDepth, *subCameraInfoColor, *subCameraInfoDepth);
+        syncExact->registerCallback(boost::bind(&Receiver::callback, this, _1, _2, _3, _4));
 
         spinner.start();
-        while(!updateImage)
+
+        cloud = pcl::PointCloud<pcl::PointXYZRGBA>::Ptr(new pcl::PointCloud<pcl::PointXYZRGBA>());
+        cloud->height = color.rows;
+        cloud->width = color.cols;
+        cloud->is_dense = false;
+        cloud->points.resize(cloud->height * cloud->width); 
+        createLookup(this->color.cols, this->color.rows); 
+
+        while(!getImage ||!updateCloud)
         {
             if(!ros::ok())
             {
@@ -105,7 +124,9 @@ class Receiver{
             }
             boost::this_thread::sleep(boost::posix_time::milliseconds(1));
         }
-        imageViewer();
+
+        get_image_thread = boost::thread(&Receiver::getImage, this);
+        cloudViewer();
     }
     void stop()
     {
@@ -117,7 +138,7 @@ class Receiver{
         delete subImageDepth;
         delete subCameraInfoColor;
         delete subCameraInfoDepth;
-
+        get_image_thread.join();
         running = false;
     }
     void callback(const sensor_msgs::Image::ConstPtr imageColor, const sensor_msgs::Image::ConstPtr imageDepth,
@@ -131,6 +152,7 @@ class Receiver{
         readImage(imageDepth, depth);
 
         // IR image input
+        // TODO: Maybe Useful for Image type conversion
         if(color.type() == CV_16U)
         {
             cv::Mat tmp;
@@ -141,11 +163,12 @@ class Receiver{
         lock.lock();
         this->color = color;
         this->depth = depth;
-        updateImage = true;
+        getImage = true;
+        updateCloud = true;
         lock.unlock();
     }
 
-    void imageViewer()
+    void getImage()
     {
         cv::Mat color, depth, depthDisp, combined;
         double fps = 0;
@@ -169,31 +192,111 @@ class Receiver{
                 depth = this->depth;
                 updateImage = false;
                 lock.unlock();
-
-                dispDepth(depth, depthDisp, 12000.0f);
-                combine(color, depthDisp, combined);
-                //combined = color;
-
-                cv::putText(combined, oss.str(), pos, font, sizeText, colorText, lineText, CV_AA);
-                cv::imshow("Image Viewer", combined);
-            }
-
-            int key = cv::waitKey(1);
-            switch(key & 0xFF)
-            {
-                case 27:
-                case 'q':
-                    running = false;
-                    break;
-                case ' ':
-                case 's':
-                    saveImages(color, depth, depthDisp);
-                    break;
             }
         }
-        cv::destroyAllWindows();
-        cv::waitKey(100);
     }
+
+    void cloudViewer()
+    {
+        cv::Mat color, depth;
+        pcl::visualization::PCLVisualizer::Ptr visualizer(new pcl::visualization::PCLVisualizer("Cloud Viewer"));
+        const std::string cloudName = "rendered";
+
+        lock.lock();
+        color = this->color;
+        depth = this->depth;
+        updateCloud = false;
+        lock.unlock();
+
+        createCloud(depth, color, cloud);
+
+        visualizer->addPointCloud(cloud, cloudName);
+        visualizer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, cloudName);
+        visualizer->initCameraParameters();
+        visualizer->setBackgroundColor(0, 0, 0);
+        visualizer->setPosition(mode == BOTH ? color.cols : 0, 0);
+        visualizer->setSize(color.cols, color.rows);
+        visualizer->setShowFPS(true);
+        visualizer->setCameraPosition(0, 0, 0, 0, -1, 0);
+        visualizer->registerKeyboardCallback(&Receiver::keyboardEvent, *this);
+
+        for(; running && ros::ok();)
+        {
+            if(updateCloud)
+            {
+                lock.lock();
+                color = this->color;
+                depth = this->depth;
+                updateCloud = false;
+                lock.unlock();
+
+                createCloud(depth, color, cloud);
+
+                visualizer->updatePointCloud(cloud, cloudName);
+            }
+            visualizer->spinOnce(10);
+        }
+        visualizer->close();
+    }
+
+    void createLookup(size_t width, size_t height)
+    {
+        const float fx = 1.0f / cameraMatrixColor.at<double>(0, 0);
+        const float fy = 1.0f / cameraMatrixColor.at<double>(1, 1);
+        const float cx = cameraMatrixColor.at<double>(0, 2);
+        const float cy = cameraMatrixColor.at<double>(1, 2);
+        float *it;
+
+        lookupY = cv::Mat(1, height, CV_32F);
+        it = lookupY.ptr<float>();
+        for(size_t r = 0; r < height; ++r, ++it)
+        {
+            *it = (r - cy) * fy;
+        }
+
+        lookupX = cv::Mat(1, width, CV_32F);
+        it = lookupX.ptr<float>();
+        for(size_t c = 0; c < width; ++c, ++it)
+        {
+            *it = (c - cx) * fx;
+        }
+    } 
+
+    void createCloud(const cv::Mat &depth, const cv::Mat &color, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr &cloud) const
+    {
+        const float badPoint = std::numeric_limits<float>::quiet_NaN();
+
+#pragma omp parallel for
+        for(int r = 0; r < depth.rows; ++r)
+        {
+            pcl::PointXYZRGBA *itP = &cloud->points[r * depth.cols];
+            const uint16_t *itD = depth.ptr<uint16_t>(r);
+            const cv::Vec3b *itC = color.ptr<cv::Vec3b>(r);
+            const float y = lookupY.at<float>(0, r);
+            const float *itX = lookupX.ptr<float>();
+
+            for(size_t c = 0; c < (size_t)depth.cols; ++c, ++itP, ++itD, ++itC, ++itX)
+            {
+                register const float depthValue = *itD / 1000.0f;
+                // Check for invalid measurements
+                if(*itD == 0)
+                {
+                    // not valid
+                    itP->x = itP->y = itP->z = badPoint;
+                    itP->rgba = 0;
+                    continue;
+                }
+                itP->z = depthValue;
+                itP->x = *itX * depthValue;
+                itP->y = y * depthValue;
+                itP->b = itC->val[0];
+                itP->g = itC->val[1];
+                itP->r = itC->val[2];
+                itP->a = 255;
+            }
+        }
+    }
+
     void readImage(const sensor_msgs::Image::ConstPtr msgImage, cv::Mat &image) const
     {
         cv_bridge::CvImageConstPtr pCvImage;
@@ -210,46 +313,6 @@ class Receiver{
         }
     }
 
-    void dispDepth(const cv::Mat &in, cv::Mat &out, const float maxValue)
-    {
-        cv::Mat tmp = cv::Mat(in.rows, in.cols, CV_8U);
-        const uint32_t maxInt = 255;
-
-#pragma omp parallel for
-        for(int r = 0; r < in.rows; ++r)
-        {
-            const uint16_t *itI = in.ptr<uint16_t>(r);
-            uint8_t *itO = tmp.ptr<uint8_t>(r);
-
-            for(int c = 0; c < in.cols; ++c, ++itI, ++itO)
-            {
-                *itO = (uint8_t)std::min((*itI * maxInt / maxValue), 255.0f);
-            }
-        }
-
-        cv::applyColorMap(tmp, out, cv::COLORMAP_JET);
-    }
-
-    void combine(const cv::Mat &inC, const cv::Mat &inD, cv::Mat &out)
-    {
-        out = cv::Mat(inC.rows, inC.cols, CV_8UC3);
-
-#pragma omp parallel for
-        for(int r = 0; r < inC.rows; ++r)
-        {
-            const cv::Vec3b
-                *itC = inC.ptr<cv::Vec3b>(r),
-                *itD = inD.ptr<cv::Vec3b>(r);
-            cv::Vec3b *itO = out.ptr<cv::Vec3b>(r);
-
-            for(int c = 0; c < inC.cols; ++c, ++itC, ++itD, ++itO)
-            {
-                itO->val[0] = (itC->val[0] + itD->val[0]) >> 1;
-                itO->val[1] = (itC->val[1] + itD->val[1]) >> 1;
-                itO->val[2] = (itC->val[2] + itD->val[2]) >> 1;
-            }
-        }
-    }
     void saveImages(const cv::Mat &color, const cv::Mat &depth, const cv::Mat &depthColored)
     {
         oss.str("");
